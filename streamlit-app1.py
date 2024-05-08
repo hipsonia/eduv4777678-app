0576a0b833ed29dcd7f0224c44e7dc958e1360eb
hipsonia <125980197+hipsonia@users.noreply.github.com>
2024-05-08 21:14:39 +0200

    Update streamlit-app1.py

--- streamlit-app1.py
+++ streamlit-app1.py
@@ -9,7 +9,7 @@ st.title("Heart Disease Prediction")
 # Sidebar with user inputs
 st.sidebar.header("User Input Features")
 # Load the trained model
-model = joblib.load('best_model_heart_disease_prediction.pkl')
+model = print('best_model_heart_disease_prediction.pkl')
 
 
 # Function to get user input
