a0ca35bfd7d96a518646fb5b115fd955903cc1a0
hipsonia <125980197+hipsonia@users.noreply.github.com>
2024-06-22 21:49:38 +0200

    Update streamlit-app4.py

--- streamlit-app4.py
+++ streamlit-app4.py
@@ -7,7 +7,8 @@ import pickle
 st.title("Heart Disease Prediction")
 
 # Load the model
-
+model = pickle.load(open("heart_disease_model.pkl", "rb"))
+    st.sidebar.success("Model loaded successfully!")
 
 # Sidebar with user inputs
 st.sidebar.header("User Input Features")
